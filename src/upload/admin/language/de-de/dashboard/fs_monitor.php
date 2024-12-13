58ea1cd7528168e3c920ef34b8ae50b240afda5d
Talgat Shashakhmetov <talgatks@gmail.com>
2024-12-13 13:58:32 -0800

    Release v1.1.2

---
+++ src/upload/admin/language/de-de/dashboard/fs_monitor.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @package		FSMonitor - German Translation by https://osworx.net
+ * @author		mic - https://osworx.net
+ * @copyright	2021 https://shtt.blog/
+ * @license		https://shtt.blog/
+ */
+
+// Heading
+$_['heading_title']    = 'FSMonitor';
+
+// Text
+$_['text_extension']   = 'Erweiterungen';
+$_['text_success']     = 'Einstellungen erfolgreich gespeichert';
+$_['text_edit']        = 'Bearbeiten';
+
+// Entry
+$_['entry_status']     = 'Status';
+$_['entry_sort_order'] = 'Reihenfolge';
+$_['entry_width']      = 'Breite';
+
+// Error
+$_['error_permission'] = 'Keine Berechtigung zum bearbeiten';
