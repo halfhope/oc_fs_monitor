4ae958f05d8b92af5b19ff72f855bf083cf6093f
Talgat Shashakhmetov <talgatks@gmail.com>
2024-12-13 13:58:50 -0800

    Release v1.2.2

--- src/upload/admin/controller/module/fs_monitor.php
+++ src/upload/admin/controller/module/fs_monitor.php
@@ -7,7 +7,7 @@ class ControllerModuleFsMonitor extends Controller {
 
 	private	$_route 			= 'module/fs_monitor';
 	private	$_model 			= 'model_module_fs_monitor';
-	private $_version 			= '1.2.1';
+	private $_version 			= '1.2.2';
 	private	$_dashboard_route	= 'dashboard/fs_monitor';
 	private	$_extensions_route 	= 'extension/module';
 
@@ -396,6 +396,12 @@ class ControllerModuleFsMonitor extends Controller {
 			$data['security_fs_cron_notify'] = $this->request->post['security_fs_cron_notify'];
 		} else {
 			$data['security_fs_cron_notify'] = $this->config->get('security_fs_cron_notify');
+		}
+
+		if (isset($this->request->post['security_fs_emails'])) {
+			$data['security_fs_emails'] = $this->request->post['security_fs_emails'];
+		} else {
+			$data['security_fs_emails'] = $this->config->get('security_fs_emails');
 		}
 
 		$data['security_fs_cron_wget'] = '/usr/local/bin/wget -q -O- \'' . str_replace($this->config->get('security_fs_admin_dir') . '/', '', HTTP_SERVER) . 'index.php?route=' . $this->_route . '&access_key=';
