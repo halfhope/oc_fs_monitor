982cdc1923017d8d992c5d38c0f2ef2a8e41a8c1
Talgat Shashakhmetov <tshashakhmetov@gmail.com>
2025-11-20 19:57:01 -0800

    Fix: Add iconv existence check

--- src/system/library/security/directory_scanner.php
+++ src/system/library/security/directory_scanner.php
@@ -117,7 +117,9 @@ class Directory_scanner {
 
 	private static function normalizePath(string $path, string $encoding = "UTF-8"): string {
 		// Attempt to avoid path encoding problems.
-		$path  = iconv($encoding, "$encoding//IGNORE//TRANSLIT", $path);
+		if (function_exists('iconv')) {
+			$path  = iconv($encoding, "$encoding//IGNORE//TRANSLIT", $path);
+		}
 		// Process the components
 		$parts = explode('/', $path);
 		$safe  = [];
